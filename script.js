e14864b8fbefa48a56be2e38cd84cede1c04e83e
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-14 20:53:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstChar(text) {
   // your code here
+	const trimmedStr = text.trim();
+
+  // Check if the string is not empty
+  if (trimmedStr.length > 0) {
+    // Return the first character
+    return trimmedStr.charAt(0);
+  } else {
+    // Return an empty string if the input is empty
+    return '';
+  }
 }
 
 // Do not change the code below
